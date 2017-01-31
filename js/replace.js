fec675167d407f44dd976e5472894a2144b080ea
yoru-san <cecel31@free.fr>
2017-01-31 14:00:56 +0100

    ajout image

--- js/replace.js
+++ js/replace.js
@@ -8,6 +8,7 @@
         polipicsImgs : {
           fn: [
             /* marine le pen*/
+            "https://media.giphy.com/media/3oriO9WHiu01Efy43m/source.gif",
             "http://media.rtl.fr/online/image/2014/0929/7774557188_marine-le-pen-le-25-juin-a-nanterre-archives.jpg",
             "http://media.rtl.fr/online/image/2014/0929/7774557188_marine-le-pen-le-25-juin-a-nanterre-archives.jpg",
             "http://cdn1-europe1.new2.ladmedia.fr/var/europe1/storage/images/europe1/politique/marine-le-pen-accuse-le-gouvernement-de-sombrer-dans-lapathie-2627585/25008965-1-fre-FR/Marine-Le-Pen-accuse-le-gouvernement-de-sombrer-dans-l-apathie.jpg",
@@ -20,20 +21,25 @@
             "http://img.humanite.fr/sites/default/files/images/lepen_3.jpg",
             "http://img.bfmtv.com/c/1000/600/80f/fd864680ac12e23d05fedd42c0956.jpg",
             "http://www.marianne.net/sites/default/files/11964-100245910.jpg",
+            "https://media.giphy.com/media/t1bSApbhk2upG/giphy.gif",
             "http://referentiel.nouvelobs.com/file/7370605-marine-le-pen-et-maintenant-objectif-elysee-2017.jpg",
+            "https://media.giphy.com/media/3o6ZtkyxtLMVpoVqbm/source.gif",
             "http://static.lexpress.fr/medias_11146/w_2048,h_1146,c_crop,x_0,y_215/w_640,h_360,c_fill,g_north/v1474893550/3404-marine-le-pen_5707213.jpg",
             "http://i.f1g.fr/media/ext/orig/www.lefigaro.fr/medias/2012/04/21/508db0ac-8be2-11e1-b0a5-27a13017d311-493x328.jpg",
             "http://www.atlantico.fr/sites/atlantico.fr/files/styles/une/public/images/2014/09/rtx17532.jpg",
             "http://referentiel.nouvelobs.com/file/3588921-marine-le-pen-est-elle-d-extreme-droite.jpg",
+            "https://media.giphy.com/media/zPUcjOyAUNFTi/giphy.gif",
             "http://humourger.com/les-pires-grimaces-des-hommes-et-femmes-politiques/Marine_Le_Pen_tire_la_langue.jpg",
             "http://cdn-europe1.new2.ladmedia.fr/var/europe1/storage/images/le-lab/front-national-vous-avez-dit-dediabolise-648/11253-1-fre-FR/Caricatures-au-FN-Marine-Le-Pen-regrette.jpeg",
             /*Jean marie le pen*/
+            "https://media.giphy.com/media/3o6ZsWt3E2MfsBsuEE/source.gif",
             "http://img.humanite.fr/sites/default/files/images/jml.jpg",
             "http://media.rtl.fr/cache/0lyHTrU7OkCPpsqi5XzxaQ/795v530-0/online/image/2015/0408/7777310232_jean-marie-le-pen-lors-d-une-conference-de-presse-a-aubigny-le-17-janvier-2015.jpg",
             "http://www.francetvinfo.fr/image/74w0kuklx-278e/1200/450/2566735.jpg",
             "http://referentiel.nouvelobs.com/file/7400487-oui-jean-marie-le-pen-a-bien-tenu-des-propos-antisemites.jpg",
             "https://etasoeurvousdittout.files.wordpress.com/2012/04/penjpg005_600.jpg",
             "http://referentiel.nouvelobs.com/wsfile/6171312296632.jpg",
+            "https://media.giphy.com/media/3o6ZtqBHg3U42oQTHq/source.gif",
             "https://oeilsurlefront.liberation.fr/images/4/2/3/3/8/42338c159d33958a5cd429f78daa502fb00f0d94-le-penazerftqrfgqzerfqzesrf.jpeg",
             "http://cdn-lejdd.ladmedia.fr/var/lejdd/storage/images/media/images/politique/jean-marie-le-pen64/12710212-1-fre-FR/Jean-Marie-Le-Pen.jpg",
             "http://s2.lemde.fr/image/2015/10/14/534x0/4789431_3_e7bd_jean-marie-le-pen-lors-d-une-conference-de_4fe023d3be10bc890fdf9da27c8a6500.jpg",
@@ -45,6 +51,7 @@
           ],
           ps: [
             /*Hollande*/
+            "http://i.imgur.com/u7VWLL7.gif",
             "http://institutdeslibertes.org/wp-content/uploads/2015/09/french-president-francois-hollande-might-give-up.jpg",
             "http://www.telegraph.co.uk/content/dam/news/2016/07/13/hollande_2661326b-xlarge_trans%2B%2BpJliwavx4coWFCaEkEsb3kvxIt-lGGWCWqwLa_RXJU8.jpg",
             "https://cdnfr1.img.sputniknews.com/images/102662/32/1026623274.jpg",
@@ -56,26 +63,31 @@
             "http://www.atlantico.fr/sites/atlantico.fr/files/styles/une/public/images/2015/04/politico-scanner-solo.jpg",
             "http://i-cms.linternaute.com/image_cms/original/10258146-francois-hollande-volte-face-ideologique-sur-la-jungle-de-calais.jpg",
             /*Valls*/
+            "https://media.giphy.com/media/l0HlUjS8qn2jiJN3G/source.gif",
             "http://www.polemia.com/wp-content/uploads/2015/05/1-Valls-paroles.jpg",
             "http://img.over-blog-kiwi.com/1/42/10/09/20150318/ob_fcbbcc_valls-bilan.png",
             "http://img.humanite.fr/sites/default/files/images/valls3005_0.jpg",
             "http://i.f1g.fr/media/figaro/805x453_crop/2016/05/23/XVM4fd2be34-20fb-11e6-85bf-82d1da223fc8.jpg",
             "https://leblogdenathaliemp.files.wordpress.com/2016/12/manuel-valls-pm-acc80-lan1.jpg",
+            "https://media.giphy.com/media/l2JhEM7RgmbOmZWtq/source.gif",
             "http://www.atlantico.fr/sites/atlantico.fr/files/styles/une/public/images/2016/01/valls.jpg",
             "http://cdn2-europe1.new2.ladmedia.fr/var/europe1/storage/images/le-lab/manuel-valls-je-ne-veux-ni-de-la-peine-de-mort-ni-de-guantanamo-ni-de-la-vente-des-armes-2772245/27579134-1-fre-FR/Manuel-Valls-Je-ne-veux-ni-de-la-peine-de-mort-ni-de-Guantanamo-ni-de-la-vente-des-armes.jpg",
             "http://www.delitdimages.org/wp-content/uploads/2014/05/valls.jpg",
+            "https://media.giphy.com/media/pVsfCJyI3TJOU/giphy.gif",
             "http://o.aolcdn.com/dims-shared/dims3/GLOB/legacy_thumbnail/630x315/format/jpg/quality/85/http%3A%2F%2Fi.huffpost.com%2Fgen%2F4697664%2Fimages%2Fn-MANUEL-VALLS-628x314.jpg",
             "http://cdn1-europe1.new2.ladmedia.fr/var/europe1/storage/images/le-lab/manuel-valls-et-des-ministres-ne-se-cachent-meme-plus-pour-critiquer-emmanuel-macron-2797603/28027258-1-fre-FR/Manuel-Valls-et-des-ministres-ne-se-cachent-meme-plus-pour-critiquer-Emmanuel-Macron.jpg",
             /*Cazeneuve*/
             "http://s1.lprs1.fr/images/2016/09/15/6122739_azeneuve_1000x625.jpg",
             "http://www.challenges.fr/assets/img/2013/03/27/cover-r4x3w1000-57988092d2b90-portrait-cazeneuve.jpg",
+            "https://media.giphy.com/media/l3q2KN5EBJ3CdWQvK/giphy.gif",
             "http://media.rtl.fr/cache/abQWJbAfa8R7DujfSKZslA/795v530-0/online/image/2015/0220/7776680497_bernard-cazeneuve-lors-d-un-discours.jpg",
+            "https://media.giphy.com/media/26gsajc8mBsdT0zFS/giphy.gif",
             "http://md1.libe.com/photo/973638-le-premier-ministre-bernard-cazeneuve-le-9-decembre-2016-a-bordeaux.jpg?modified_at=1481313041&width=960",
             "http://www.lopinion.fr/sites/nb.com/files/styles/w_1000/public/styles/paysage/public/cms/edito/201411/cazeneuve_pays_sipa.jpg?itok=qh_r9Zdb",
             "http://s-www.republicain-lorrain.fr/images/9D5B334C-0870-4FD6-969E-CAE7F157DF53/LRL_v0_03/bernard-cazeneuve-ne-gere-pas-lui-meme-sonc-ompte-twitter-a-fait-savoir-son-equipe-photo-afp-1463408416.jpg",
             "http://md1.libe.com/photo/520421-le-ministre-delegue-au-budget-bernard-cazeneuve-le-22-mai-2013-a-caen.jpg?modified_at=1369992045&width=750"
           ],
-          lr: [
+          rep: [
             //Nicolas Sarkozy
             "http://www.dreuz.info/wp-content/uploads/2016/09/sarkozy.jpg",
             "http://referentiel.nouvelobs.com/file/rw765h740/15512501-sarkozy-intouchable-pourquoi-nous-sommes-tous-coupables.jpg",
@@ -104,8 +116,7 @@
             "https://media1.giphy.com/media/3o7TKDbNOfAntlCVZm/200.gif#18",
             "https://media1.giphy.com/media/3o7TKwKC8W7gUmFnxe/200.gif#20",
             "https://media2.giphy.com/media/l0MYIYA1DYi4Lxwvm/200.gif#48",
-            "https://media.giphy.com/media/2QIYRKSgfY1ig/giphy.gif", 
-              //Francois Fillon
+            "https://media.giphy.com/media/2QIYRKSgfY1ig/giphy.gif", //Francois Fillon
             "http://cdn-lejdd.ladmedia.fr/var/lejdd/storage/images/media/images/politique/francois-fillon-a-demande-a-ses-soutiens-de-se-preparer/12696484-1-fre-FR/Francois-Fillon-a-demande-a-ses-soutiens-de-se-preparer.jpg",
             "http://beninmondeinfos.com/images/FillonFranois.jpg",
             "https://comptoirdotorg.files.wordpress.com/2016/11/fillon.jpg",
@@ -126,18 +137,44 @@
           ],
           fi: [
             /*Mélenchon*/
+            "https://media.giphy.com/media/3oriO5Dg62yptDQXDO/source.gif",
             "http://www.francetvinfo.fr/image/759ujl2rb-7891/1500/843/11056201.jpg",
             "http://www.lepoint.fr/images/2016/12/20/6492437lpw-6496497-article-francevotepg-jpg_3974542_660x281.jpg",
             "http://static.lexpress.fr/medias/1188/608363_social-retraites-partis-gauche.jpg",
             "http://media.rtl.fr/online/image/2013/1201/7767489565_jean-luc-melenchon-le-29-novembre-2013-a-paris.jpg",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Jean-Luc_M%C3%A9lenchon_-_avril_2012.jpg/220px-Jean-Luc_M%C3%A9lenchon_-_avril_2012.jpg",
+            "https://media.giphy.com/media/3oz8xvjCx3BHSOPAXu/source.gif",
             "http://www.prechi-precha.fr/wp-content/uploads/2016/09/3170150_3_2bad_jean-luc-melenchon-lors-d-un-meeting-de_9e7e15d3e5f98f2938218290579cfb96.jpg",
             "http://www.mouv.fr/sites/default/files/2012/10/12/37976/MELENCHON.jpg",
+            "https://media.giphy.com/media/f0eIzwnD5c5BC/giphy.gif",
             "http://cdn1-europe1.new2.ladmedia.fr/var/europe1/storage/images/le-lab/jean-luc-melenchon-sen-prend-aux-journalistes-deuxieme-peau-du-systeme-que-combat-le-mouvement-nuit-debout-2713618/26518357-1-fre-FR/Jean-Luc-Melenchon-s-en-prend-aux-journalistes-deuxieme-peau-du-systeme-que-combat-le-mouvement-Nuit-Debout.jpg",
             "http://cdn-europe1.new2.ladmedia.fr/var/europe1/storage/images/europe1/politique/pour-lentourage-de-melenchon-labsence-de-soutien-du-pcf-ne-change-rien-2892110/29681084-1-fre-FR/Pour-l-entourage-de-Melenchon-l-absence-de-soutien-du-PCF-ne-change-rien.jpg",
             "http://www.challenges.fr/assets/img/2016/02/11/cover-r4x3w1000-578e59b921143-melenchon.jpg",
             "http://www.atlantico.fr/sites/atlantico.fr/files/styles/une/public/images/2014/04/rtxz6ar.jpg",
             "http://cdn1-europe1.new2.ladmedia.fr/var/europe1/storage/images/le-lab/les-bonnes-resolutions-philosophico-politiques-de-jean-luc-melenchon-pour-2016-2643567/25306953-1-fre-FR/Les-bonnes-resolutions-philosophico-politiques-de-Jean-Luc-Melenchon-pour-2016.jpg",
+            "http://s-www.republicain-lorrain.fr/images/293DD13F-6241-45EF-8A77-25D60A49A051/LRL_v0_13b/jean-luc-melenchon-etait-venu-soutenir-les-salaries-d-arcelormittal-en-lutte-pour-le-maintien-des-hauts-fourneaux-en-decembre-2012-photo-philippe-neu-1484682388.jpg",
+            "http://referentiel.nouvelobs.com/file/15603187-presidentielle-pourquoi-les-communistes-ont-lache-melenchon-et-contredit-leur-chef.jpg",
+            "http://img2.closermag.fr/var/closermag/storage/images/media/images-des-contenus/article/2016-09-08-jean-luc-melenchon-se-confie-sur-son-handicap-physique/jean-luc-melenchon-responsable-du-parti-de-gauche-et-candidat-aux-elections-presidentielles-visite5/5459666-3-fre-FR/Jean-Luc-Melenchon-responsable-du-Parti-de-Gauche-et-candidat-aux-elections-presidentielles-visite_exact1024x768_l.jpg",
+            "http://www.lepoint.fr/images/2014/07/23/melenchon-2755101-jpg_2389409_660x281.JPG",
+            "https://i0.wp.com/mcetv.fr/wp-content/uploads/2016/03/Jean-Luc-Me%CC%81lenchon-favorable-a%CC%80-un-service-civique-national-proche-du-service-militaire.jpg?fit=640%2C360",
+            "http://s-www.estrepublicain.fr/images/3E08093C-A2F9-488C-83C5-20AC8F11C322/LER_22/melenchon-un-tribun-hors-pair-photo-afp-1481203878.jpg",
+            "https://94.citoyens.com/wp-content/blogs.dir/2/files/2016/05/jean-luc-melenchon.jpg",
+            "http://lagauchematuer.fr/wp-content/uploads/2016/06/m%C3%A9lenchon.jpg",
+            "https://media.giphy.com/media/3oz8xMfiOXldHb7K5W/source.gif",
+            "http://s2.lemde.fr/image/2011/12/01/534x267/1611799_3_9892_jean-luc-melenchon-le-candidat-du-front-de_4afb4c8f985f05e3a15fef9300594fd0.jpg",
+            "http://md1.libe.com/photo/882416-prodlibe.jpg?modified_at=1465151626&width=960",
+            "http://referentiel.nouvelobs.com/file/3836840-henin-beaumont-le-pen-en-tete-melenchon-reconnait-sa-defaite.jpg",
+            "http://www.lepoint.fr/images/2010/10/12/179408-melenchonuen-jpg_76596.jpg",
+            "http://img.humanite.fr/sites/default/files/legacy/melenchonbis.jpg",
+            "http://www.nouvelordremondial.cc/wp-content/uploads/2012/04/meluche.jpg",
+            "https://media.giphy.com/media/3o7TKQcgbgPy80Y5IA/source.gif",
+            "http://cdn2-europe1.new2.ladmedia.fr/var/europe1/storage/images/le-lab/quand-jean-luc-melenchon-prevoit-d-etre-mis-en-garde-a-vue-lors-de-sa-manifestation-du-5-mai-8504/160517-1-fre-FR/Quand-Jean-Luc-Melenchon-prevoit-d-etre-mis-en-garde-a-vue-lors-de-sa-manifestation-du-5-mai.jpeg",
+            "http://img.gal.pmdstatic.net/fit/http.3A.2F.2Fwww.2Egala.2Efr.2Fvar.2Fgal.2Fstorage.2Fimages.2Fmedia.2Fmultimedia.2Fteddy_riner_invite_melenchon_sur_un_tatami.2F3374334-1-fre-FR.2Fteddy_riner_invite_melenchon_sur_un_tatami.2Ejpg/1140x499/crop-from/top/teddy-riner-invite-melenchon-sur-un-tatami.jpg",
+            "http://www.bestbuzz.fr/wp-content/uploads/2012/06/m%C3%A9lenchon.jpg",
+            "https://media.giphy.com/media/l3vRnI8grVFLqcC5i/source.gif",
+            "http://www.annuaire-voyance-symphony.com/athina-odias/images/images-etudes/jlm.jpg",
+            "http://3.bp.blogspot.com/-IAsM4AGmOoA/U_c0VctjocI/AAAAAAAAATI/wweUmmtONcE/s1600/jean-luc-melenchon.jpg"
+          ]
             /*Staline*/
             "https://www.world-lolo.com/images/uploads/image.num1470699041.of.world-lolo.com.jpg",
             "http://1.bp.blogspot.com/-bG4xB-9a4nw/U8rPXkttfyI/AAAAAAAAACg/Tf5N3S5EKBY/s1600/Staline.jpg",
@@ -165,7 +202,7 @@
 					else
 					{
 						//Replace when loaded
-						$(item).on('load', function(){
+						$(item).load(function(){
 							//Prevent 'infinite' loop
 							if($.inArray($(item).attr('src'), lstImgs) == -1)
 							{
